be81cec83003884eb1ea46ce83e61294d03b31e2
suteado90573 <suteado90573@gmail.com>
2025-10-05 11:07:48 +0900

    #9 CI設定を一部修正

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({ dir: './' })
+
+const customJestConfig = {
+  testEnvironment: 'jest-environment-jsdom',
+  coverageProvider: 'v8',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+}
+
+module.exports = createJestConfig(customJestConfig)
